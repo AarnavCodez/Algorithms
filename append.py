688b8eb9b9da7d81b97c8232da4b4c0b66f6c97c
AarnavCodez <aarnavk23044441@gmail.com>
2024-08-08 18:44:14 -0400

    Version 0.2 Added these 2 new files: Append and Insert!

---
+++ append.py
@@ -0,0 +1,9 @@
+# My append function. Works like the normal one
+
+def my_append(array, element):
+    new_array = [0 for i in range(len(array) + 1)]
+    # print(new_array)
+    for i in range(0, len(array)):
+        new_array[i] = array[i]
+    new_array[len(array)] = element
+    return new_array
