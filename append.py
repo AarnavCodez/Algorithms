1a8d4ac87caf3b2f1c530040e1ea06abd84f5bfe
AarnavCodez <aarnavk23044441@gmail.com>
2024-08-15 15:32:45 -0400

    Update append.py

--- append.py
+++ append.py
@@ -1,6 +1,7 @@
 # My append function. Works like the normal one
 
 def my_append(array, element):
+    """Appends an element at the end of a list"""
     new_array = [0 for _ in range(len(array) + 1)]
     # print(new_array)
     for i in range(0, len(array)):
